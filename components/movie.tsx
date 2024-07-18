bc6b35772f9d3a486f1a10104530664229a0d4a1
LazyMG <cbfmark@gmail.com>
2024-07-18 18:43:03 +0900

    deploy

---
+++ components/movie.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "../styles/movie.module.css";
+
+interface MovieProps {
+  title: string;
+  id: number;
+  poster_path: string;
+}
+
+const Movie = ({ title, id, poster_path }: MovieProps) => {
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+};
+
+export default Movie;
